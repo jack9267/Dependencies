a3324d471b04e4524ac41147532d746c9fafbaeb
Jack Powell <jack9267@users.noreply.github.com>
2019-01-17 19:26:40 +0000

    Disable enhanced instructions for XP targetting builds

--- Compile.bat
+++ Compile.bat
@@ -7,7 +7,7 @@ cd %1
 mkdir %2
 cd %2
 
-cmake ../../.. -G %1 -T %2 -DCMAKE_INSTALL_PREFIX="../../../Build"
+cmake ../../.. -G %1 -T %2 -DCMAKE_INSTALL_PREFIX="../../../Build" %3
 if %ERRORLEVEL% neq 0 pause && goto eof
 cmake --build . --config Debug --target install
 if %ERRORLEVEL% neq 0 pause && goto eof
@@ -18,7 +18,7 @@ cd ..
 mkdir %2_static
 cd %2_static
 
-cmake ../../.. -G %1 -T %2 -DCMAKE_INSTALL_PREFIX="../../../Build" -DFORCE_STATIC_VCRT=ON
+cmake ../../.. -G %1 -T %2 -DCMAKE_INSTALL_PREFIX="../../../Build" -DFORCE_STATIC_VCRT=ON %3
 if %ERRORLEVEL% neq 0 pause && goto eof
 cmake --build . --config Debug --target install
 if %ERRORLEVEL% neq 0 pause && goto eof
--- VS2015_XP.bat
+++ VS2015_XP.bat
@@ -1,2 +1,2 @@
 @echo off
-call Compile.bat "Visual Studio 14 2015" "v140_xp"
+call Compile.bat "Visual Studio 14 2015" "v140_xp" -DNO_ENHANCED_INSTRUCTIONS=ON
