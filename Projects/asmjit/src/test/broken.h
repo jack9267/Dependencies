fae226264a87ffc59eb50b8962df2d78b159fffd
Jack Powell <jack9267@users.noreply.github.com>
2020-04-30 22:53:11 +0100

    Update asmjit to 1.2.0

--- Projects/asmjit/src/test/broken.h
+++ Projects/asmjit/src/test/broken.h
@@ -2,43 +2,20 @@
 // Lightweight Unit Testing for C++.
 //
 // [License]
-// Public Domain (Unlicense)
+// Public Domain (Unlicense) or Zlib.
 
-// [Guard]
 #ifndef BROKEN_INTERNAL_H
 #define BROKEN_INTERNAL_H
 
-// [Dependencies]
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// ============================================================================
-// [Broken - Detection]
-// ============================================================================
-
-#if defined(__GNUC__) && defined(__GNUC_MINOR__)
-# if (__GNUC__ * 1000 + __GNUC_MINOR__) >= 3004
-#  define BROKEN_NOINLINE __attribute__((__noinline__))
-# endif
-#elif defined(__clang__)
-# if __has_attribute(__noinline__)
-#  define BROKEN_NOINLINE __attribute__((__noinline__))
-# endif
-#elif defined(_MSC_VER)
-# define __declspec(noinline)
-#endif
-
-#if !defined(BROKEN_NOINLINE)
-# define BROKEN_NOINLINE
-#endif
+#include <utility>
 
 // Hide everything when using Doxygen. Ideally this can be protected by a macro,
 // but there is not globally and widely used one across multiple projects.
 
-//! \internal
-//! \{
+//! \cond
 
 // ============================================================================
 // [Broken - API]
@@ -48,98 +25,103 @@ struct BrokenAPI {
   //! Entry point of a unit test defined by `UNIT` macro.
   typedef void (*Entry)(void);
 
+  enum Flags : unsigned {
+    kFlagFinished = 0x1
+  };
+
   //! Test defined by `UNIT` macro.
   struct Unit {
-    const char* name;
     Entry entry;
-    size_t finished;
+    const char* name;
+    int priority;
+    unsigned flags;
     Unit* next;
   };
 
   //! Automatic unit registration by using static initialization.
   struct AutoUnit : Unit {
-    inline AutoUnit(const char* _name, Entry _entry) {
-      name = _name;
-      entry = _entry;
-      finished = false;
-      next = NULL;
+    inline AutoUnit(Entry entry_, const char* name_, int priority_ = 0, int dummy_ = 0) noexcept {
+      // Not used, only to trick `UNIT()` macro.
+      (void)dummy_;
 
+      this->entry = entry_;
+      this->name = name_;
+      this->priority = priority_;
+      this->flags = 0;
+      this->next = nullptr;
       BrokenAPI::add(this);
     }
   };
 
+  static bool hasArg(const char* name) noexcept;
+
   //! Register a new unit test (called automatically by `AutoUnit` and `UNIT`).
-  static void add(Unit* unit);
+  static void add(Unit* unit) noexcept;
 
   //! Set output file to a `file`.
-  static void setOutputFile(FILE* file);
-
-  //! Set the current context to `file` and `line`.
-  //!
-  //! This is called by `EXPECT` macro to set the correct `file` and `line`,
-  //! because `EXPECT` macro internally calls `expect()` function, which does
-  //! change the original file & line to non-interesting `broken.h`.
-  static int setContext(const char* file, int line);
+  static void setOutputFile(FILE* file) noexcept;
 
   //! Initialize `Broken` framework.
   //!
   //! Returns `true` if `run()` should be called.
-  static int run(int argc, const char* argv[],
-    Entry onBeforeRun = (Entry)NULL,
-    Entry onAfterRun = (Entry)NULL);
+  static int run(int argc, const char* argv[], Entry onBeforeRun = nullptr, Entry onAfterRun = nullptr);
+
+  //! Log message, adds automatically new line if not present.
+  static void info(const char* fmt, ...) noexcept;
+
+  //! Called on `EXPECT()` failure.
+  static void fail(const char* file, int line, const char* expression, const char* fmt, ...) noexcept;
 
   //! Used internally by `EXPECT` macro.
   template<typename T>
-  BROKEN_NOINLINE static int expect(const T& exp, const char* fmt = NULL, ...) {
-    if (exp)
-      return 1;
-
-    va_list ap;
-    va_start(ap, fmt);
-    fail(fmt, ap);
-    va_end(ap);
-    return 0;
+  static inline void expect(const char* file, int line, const char* expression, const T& result) noexcept {
+    if (!result)
+      fail(file, line, expression, nullptr);
   }
 
-  //! Log message, adds automatically new line if not present.
-  static int info(const char* fmt, ...);
-  //! Called on `EXPECT()` failure.
-  static int fail(const char* fmt, va_list ap);
+  //! Used internally by `EXPECT` macro.
+  template<typename T, typename... Args>
+  static inline void expect(const char* file, int line, const char* expression, const T& result, const char* fmt, Args&&... args) noexcept {
+    if (!result)
+      fail(file, line, expression, fmt, std::forward<Args>(args)...);
+  }
 };
 
 // ============================================================================
 // [Broken - Macros]
 // ============================================================================
 
-//! Define a unit test.
-//!
-//! `_Name_` can only contain ASCII characters, numbers and underscore. It has
-//! the same rules as identifiers in C and C++.
-#define UNIT(_Name_) \
-  static void unit_##_Name_##_entry(void); \
-  \
-  static ::BrokenAPI::AutoUnit unit_##_Name_##_autoinit( \
-    #_Name_, unit_##_Name_##_entry); \
-  \
-  static void unit_##_Name_##_entry(void)
+//! Internal macro used by `UNIT()`.
+#define BROKEN_UNIT_INTERNAL(NAME, PRIORITY) \
+  static void unit_##NAME##_entry(void); \
+  static ::BrokenAPI::AutoUnit unit_##NAME##_autoinit(unit_##NAME##_entry, #NAME, PRIORITY); \
+  static void unit_##NAME##_entry(void)
 
-//! #define INFO(...)
+//! Stringifies the expression used by EXPECT().
+#define BROKEN_STRINFIGY_EXPRESSION_INTERNAL(EXP, ...) #EXP
+
+//! \def UNIT(NAME [, PRIORITY])
+//!
+//! Define a unit test with an optional priority.
+//!
+//! `NAME` can only contain ASCII characters, numbers and underscore. It has
+//! the same rules as identifiers in C and C++.
+//!
+//! `PRIORITY` specifies the order in which unit tests are run. Lesses value
+//! increases the priority. At the moment all units are first sorted by
+//! priority and then by name - this makes the run always deterministic.
+#define UNIT(NAME, ...) BROKEN_UNIT_INTERNAL(NAME, __VA_ARGS__ + 0)
+
+//! #define INFO(FORMAT [, ...])
 //!
 //! Informative message printed to `stdout`.
-#define INFO ::BrokenAPI::setContext(__FILE__, __LINE__) && ::BrokenAPI::info
+#define INFO(...) ::BrokenAPI::info(__VA_ARGS__)
 
-//! #define INFO(_Exp_ [, _Format_ [, ...]])
+//! #define INFO(EXP [, FORMAT [, ...]])
 //!
-//! Expect `_Exp_` to be true or evaluates to true, fail otherwise.
-#define EXPECT ::BrokenAPI::setContext(__FILE__, __LINE__) && ::BrokenAPI::expect
+//! Expect `EXP` to be true or evaluates to true, fail otherwise.
+#define EXPECT(...) ::BrokenAPI::expect(__FILE__, __LINE__, BROKEN_STRINFIGY_EXPRESSION_INTERNAL(__VA_ARGS__), __VA_ARGS__)
 
-// ============================================================================
-// [Broken - Cleanup]
-// ============================================================================
+//! \endcond
 
-#undef BROKEN_NOINLINE
-
-//! \}
-
-// [Guard]
 #endif // BROKEN_INTERNAL_H
